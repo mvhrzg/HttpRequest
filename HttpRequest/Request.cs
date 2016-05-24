011944fc30d181292fb5963d5cebd64fb7ec8f66
Mariana Herzog <mvher@DESKTOP-FQSOTRD>
2016-05-24 19:47:15 -0400

    webclient getting same html response

--- HttpRequest/Request.cs
+++ HttpRequest/Request.cs
@@ -11,98 +11,87 @@ namespace HttpRequest
 {
     class HttpRequest
     {
-        public string url = "http://oneport.unca.edu";
-        public string referer = "https://oneport.unca.edu/group/students/home/";
-        public string username;
-        public string password;
+        public string url =     "https://oneport.unca.edu/cas/login?service=https%3A%2F%2Foneport.unca.edu%2Fc%2Fportal%2Flogin";
+        public string username = "mherzog";
+        public string password = "*H00a33t00e44!";
+
+        static byte[] GetBytes(string str)
+        {
+            byte[] bytes = new byte[str.Length * sizeof(char)];
+            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
 
         public void CreateRequest()
         {
-            //Create GET request for oneport.unca.edu
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            request.Host = "oneport.unca.edu";
-            
-            //Set request referer
-            request.Referer = referer;
-
-            //Get username and password
-            Console.Write("Enter username: ");
-            username = Console.ReadLine();
-            Console.Write("Enter password: ");
-            password = Console.ReadLine();
-
-            //Set username and password
-            CredentialCache.DefaultNetworkCredentials.UserName = username;
-            CredentialCache.DefaultNetworkCredentials.Password = password;
-
-            //Set up POST request
-            var postData = username + password;
-            var data = Encoding.ASCII.GetBytes(postData);
-
-            //Set request credentials to above
-
-            //Set user-agent from postman
-            request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36";
-
-            //Create request method
-            request.Method = "POST";
-
-            //Set content type
-            request.ContentType = "application/x-www-form-urlencoded";//"text/plain;charset=UTF-8";
-
-
-            request.Method = "POST";
-            request.ContentLength = data.Length;
-
-            using (var stream = request.GetRequestStream())
-            {
-                stream.Write(data, 0, data.Length);
-            }
+            // Create web client simulating IE6.
+            WebClient client = new WebClient();
+            //CredentialCache.DefaultNetworkCredentials.Password = password;
+            //CredentialCache.DefaultNetworkCredentials.UserName = username;
+            client.Headers["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+            client.Headers["Origin"] = "https://oneport.unca.edu";
+            client.Headers["Upgrade-Insecure-Requests"] = "1";
+            client.Headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36";
+            client.Headers["Content-Type"] = "application/x-www-form-urlencoded";
+            client.Headers["Referer"] = url;
+            client.Headers["Accept-Encoding"] = "gzip, deflate";
+            client.Headers["Accept-Language"] = "en-US,en;q=0.8,pt;q=0.6";
+            client.Headers["Cookie"] = "JSESSIONID=13FA0A5BD4752232BA1A6AD324D91549; _ga=GA1.2.827633366.1464042219; viewedAlerts=true; JSESSIONID=FF36F39B87A3EBB1851BDAB64C713F8F";
             try
             {
-                request.Credentials = CredentialCache.DefaultNetworkCredentials;
-                var response = (HttpWebResponse)request.GetResponse();
-                
-                var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
-                Console.Write(responseString.ToString());
-                if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+                //client.ResponseHeaders["_eventId=submit&lt="] = "_c2C4C481B-F72C-564D-F09E-DEFAF289975D_kA73603A1-82C7-B2A6-B2EC-132FA9638E94&password=*H00a33t00e44!&submit=Verifying+Credentials...&username=mherzog";
+                //client.Credentials = CredentialCache.DefaultNetworkCredentials;
+                Byte[] pageData = client.DownloadData(url);
+                string pageHtml = Encoding.ASCII.GetString(pageData);
+                for (int i = 0; i < client.ResponseHeaders.Count; i++)
                 {
-                    Process.GetCurrentProcess().Kill();
+                    Console.Write("Reponse headers: " + client.ResponseHeaders[i] + "\n");
                 }
+                Console.WriteLine(pageHtml);
+                Console.ReadKey();
+
             }
-            catch (WebException we)
+            catch(WebException we)
             {
-                Console.Write(we.Message);
+                Console.WriteLine($"Error: {we.Message}");
+                Console.ReadKey();
             }
 
 
+            Console.Write(client.ToString());
+            Console.ReadKey();
+	        
+             //   client.Headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36";
+             //   //client.Headers["Content-Type"] = "application/x-www-form-urlencoded";
+             //   //client.Headers["Referer"] = referer;
+             //   //client.UseDefaultCredentials = true;
+             //   //client.Credentials
 
-            //    try
-            //    {
-            //        Stream objStream = request.GetResponse().GetResponseStream();
-            //        StreamReader objReader = new StreamReader(objStream);
-            //        string sLine = "";
-            //        int i = 0;
+             //   // Download data.
+             //   byte[] arr = client.DownloadData(url);
 
-            //        while (sLine != null)
-            //        {
-            //            i++;
-            //            sLine = objReader.ReadLine();
-            //            //if (sLine != null)
-            //            Console.WriteLine("{0}:{1}", i, sLine);
-            //            //Close console if ESC
-            //            if (Console.ReadKey(true).Key == ConsoleKey.Escape)
-            //            {
-            //                Process.GetCurrentProcess().Kill();
-            //            }
-            //        }
-            //    }
-            //    catch(WebException we)
-            //    {
-            //        Console.WriteLine(we.Message);
-            //        Console.ReadKey();
-            //    }
+             //   // Get response header.
+             //   string contentEncoding = client.ResponseHeaders["password"];
+
+             //   // Write values.
+             //   Console.WriteLine("--- WebClient result ---");
+	            //Console.WriteLine(arr.Length);
+             //   string s = ;// ByteArrayToString(arr);
+             //   Console.WriteLine(s.ToString());
+             //   Console.WriteLine(contentEncoding);
+             //   if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+             //   {
+             //       Process.GetCurrentProcess().Kill();
+             //   }
         }
+
+        //public string ByteArrayToString(byte[] ba)
+        //{
+        //    StringBuilder hex = new StringBuilder(ba.Length * 2);
+        //    foreach (byte b in ba)
+        //        hex.AppendFormat("{0:x2}", b);
+        //    return hex.ToString();
+        //}
         static void Main(string[] args)
         {
             HttpRequest r = new HttpRequest();
