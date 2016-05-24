a3ba095494a64171fdb243bd4c87de546b4919b3
Mariana Herzog <mvher@DESKTOP-FQSOTRD>
2016-05-24 18:29:38 -0400

    POST httpWebRequest. not working

--- HttpRequest/Request.cs
+++ HttpRequest/Request.cs
@@ -11,15 +11,21 @@ namespace HttpRequest
 {
     class HttpRequest
     {
-        public string url = "https://oneport.unca.edu/group/students/home/";//"http://oneport.unca.edu";
+        public string url = "http://oneport.unca.edu";
+        public string referer = "https://oneport.unca.edu/group/students/home/";
         public string username;
         public string password;
 
         public void CreateRequest()
         {
             //Create GET request for oneport.unca.edu
-            WebRequest request = WebRequest.Create(url);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Host = "oneport.unca.edu";
+            
+            //Set request referer
+            request.Referer = referer;
 
+            //Get username and password
             Console.Write("Enter username: ");
             username = Console.ReadLine();
             Console.Write("Enter password: ");
@@ -28,48 +34,76 @@ namespace HttpRequest
             //Set username and password
             CredentialCache.DefaultNetworkCredentials.UserName = username;
             CredentialCache.DefaultNetworkCredentials.Password = password;
+
+            //Set up POST request
+            var postData = username + password;
+            var data = Encoding.ASCII.GetBytes(postData);
+
             //Set request credentials to above
-            request.Credentials = CredentialCache.DefaultNetworkCredentials;
 
             //Set user-agent from postman
-            ((HttpWebRequest)request).UserAgent = " Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36";
+            request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36";
 
-            //Create POST request
-            request.Method = "post";
-
-            //Set content length
-            //request.ContentLength = 3030;
+            //Create request method
+            request.Method = "POST";
 
             //Set content type
-            request.ContentType = "text/plain;charset=UTF-8";
+            request.ContentType = "application/x-www-form-urlencoded";//"text/plain;charset=UTF-8";
 
+
+            request.Method = "POST";
+            request.ContentLength = data.Length;
+
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
             try
             {
-                Stream objStream = request.GetResponse().GetResponseStream();
-                StreamReader objReader = new StreamReader(objStream);
-                string sLine = "";
-                int i = 0;
-
-                while (sLine != null)
+                request.Credentials = CredentialCache.DefaultNetworkCredentials;
+                var response = (HttpWebResponse)request.GetResponse();
+                
+                var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                Console.Write(responseString.ToString());
+                if (Console.ReadKey(true).Key == ConsoleKey.Escape)
                 {
-                    i++;
-                    sLine = objReader.ReadLine();
-                    //if (sLine != null)
-                    Console.WriteLine("{0}:{1}", i, sLine);
-                    //Close console if ESC
-                    if (Console.ReadKey(true).Key == ConsoleKey.Escape)
-                    {
-                        Process.GetCurrentProcess().Kill();
-                    }
+                    Process.GetCurrentProcess().Kill();
                 }
             }
-            catch(WebException we)
+            catch (WebException we)
             {
-                Console.WriteLine(we.Message);
-                Console.ReadKey();
+                Console.Write(we.Message);
             }
+
+
+
+            //    try
+            //    {
+            //        Stream objStream = request.GetResponse().GetResponseStream();
+            //        StreamReader objReader = new StreamReader(objStream);
+            //        string sLine = "";
+            //        int i = 0;
+
+            //        while (sLine != null)
+            //        {
+            //            i++;
+            //            sLine = objReader.ReadLine();
+            //            //if (sLine != null)
+            //            Console.WriteLine("{0}:{1}", i, sLine);
+            //            //Close console if ESC
+            //            if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+            //            {
+            //                Process.GetCurrentProcess().Kill();
+            //            }
+            //        }
+            //    }
+            //    catch(WebException we)
+            //    {
+            //        Console.WriteLine(we.Message);
+            //        Console.ReadKey();
+            //    }
         }
-		static void Main(string[] args)
+        static void Main(string[] args)
         {
             HttpRequest r = new HttpRequest();
             r.CreateRequest();
