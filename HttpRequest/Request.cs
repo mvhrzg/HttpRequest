f247da09cf0cc68bf50b9c253db3c6a3481b7bf8
Mariana Herzog <mvher@DESKTOP-FQSOTRD>
2016-05-24 23:00:19 -0400

    Starting restSharp application

--- HttpRequest/Request.cs
+++ HttpRequest/Request.cs
@@ -6,103 +6,37 @@ using System.Threading.Tasks;
 using System.Net;
 using System.IO;
 using System.Diagnostics;
+using RestSharp;
+using RestSharp.Authenticators;
 
 namespace HttpRequest
 {
     class HttpRequest
     {
-        public string url = "http://oneport.unca.edu";
-        public string referer = "https://oneport.unca.edu/group/students/home/";
-        public string username;
-        public string password;
-
+        public string username = "mherzog";
+        public string password = "*H00a33t00e44!";
         public void CreateRequest()
         {
-            //Create GET request for oneport.unca.edu
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            request.Host = "oneport.unca.edu";
-            
-            //Set request referer
-            request.Referer = referer;
-
-            //Get username and password
-            Console.Write("Enter username: ");
-            username = Console.ReadLine();
-            Console.Write("Enter password: ");
-            password = Console.ReadLine();
-
-            //Set username and password
-            CredentialCache.DefaultNetworkCredentials.UserName = username;
-            CredentialCache.DefaultNetworkCredentials.Password = password;
-
-            //Set up POST request
-            var postData = username + password;
-            var data = Encoding.ASCII.GetBytes(postData);
-
-            //Set request credentials to above
-
-            //Set user-agent from postman
-            request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36";
-
-            //Create request method
-            request.Method = "POST";
-
-            //Set content type
-            request.ContentType = "application/x-www-form-urlencoded";//"text/plain;charset=UTF-8";
+            var client = new RestClient();
+            client.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36";
+            //for(int i = 0; i < client.DefaultParameters.Count; i++)
+            //{
+            //    Console.Write(client.DefaultParameters[i].ToString());
+            //}
+            Console.Write(client.HttpFactory);
+            client.BaseUrl = new Uri("http://oneport.unca.edu");
+            client.Authenticator = new HttpBasicAuthenticator(username, password);
 
 
-            request.Method = "POST";
-            request.ContentLength = data.Length;
+            var request = new RestRequest(Method.GET);
+            //request.Resource =  "statuses/friends_timeline.xml";
 
-            using (var stream = request.GetRequestStream())
-            {
-                stream.Write(data, 0, data.Length);
-            }
-            try
-            {
-                request.Credentials = CredentialCache.DefaultNetworkCredentials;
-                var response = (HttpWebResponse)request.GetResponse();
-                
-                var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
-                Console.Write(responseString.ToString());
-                if (Console.ReadKey(true).Key == ConsoleKey.Escape)
-                {
-                    Process.GetCurrentProcess().Kill();
-                }
-            }
-            catch (WebException we)
-            {
-                Console.Write(we.Message);
-            }
+            IRestResponse response = client.Execute(request);
 
-
-
-            //    try
-            //    {
-            //        Stream objStream = request.GetResponse().GetResponseStream();
-            //        StreamReader objReader = new StreamReader(objStream);
-            //        string sLine = "";
-            //        int i = 0;
-
-            //        while (sLine != null)
-            //        {
-            //            i++;
-            //            sLine = objReader.ReadLine();
-            //            //if (sLine != null)
-            //            Console.WriteLine("{0}:{1}", i, sLine);
-            //            //Close console if ESC
-            //            if (Console.ReadKey(true).Key == ConsoleKey.Escape)
-            //            {
-            //                Process.GetCurrentProcess().Kill();
-            //            }
-            //        }
-            //    }
-            //    catch(WebException we)
-            //    {
-            //        Console.WriteLine(we.Message);
-            //        Console.ReadKey();
-            //    }
+            Console.WriteLine(response.Content);
+            Console.ReadLine();
         }
+
         static void Main(string[] args)
         {
             HttpRequest r = new HttpRequest();
